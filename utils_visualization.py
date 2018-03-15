7e91e4033c8fb5724bf04723d1d4dd87d19765f2
Bryan Fry <bryanfry@myhost.local>
2018-03-15 15:58:13 -0400

    vis

--- utils_visualization.py
+++ utils_visualization.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 MATPLOTLIB_DFLT_COLOR_CYCLE = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 
-def apply_plot_cosmetics(ax, x_label='Frequency [Hz]', y_label=None, title=None,
-                         show_legend=True):
+def apply_plot_cosmetics(ax, x_label=None, y_label=None, title=None, show_legend=True):
     if x_label:
         ax.set_xlabel(x_label, fontsize=18)
     if y_label:
@@ -13,6 +12,6 @@ def apply_plot_cosmetics(ax, x_label='Frequency [Hz]', y_label=None, title=None,
     if title:
         ax.set_title(title, fontsize=22)
     if show_legend:
-        ax.legend(fontsize=18, frameon=True, framealpha=0.9, edgecolor='gray')
+        ax.legend(fontsize=16, frameon=True, framealpha=0.8, edgecolor='gray')
     ax.tick_params(axis='both', labelsize=14, pad=10)
     return ax
